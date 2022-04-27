83e0362b7471c7d2583491e386684f071026a295
Aria(김인숙) <shoo7830@gmail.com>
2022-04-27 16:05:34 +0900

    add: firebase 연동

---
+++ src/firebase.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+
+const firebaseConfig = {
+    apiKey: process.env.REACT_APP_APIKEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_PROJECTID,
+    storageBucket: process.env.REACT_APP_STORAGEBUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGINGSENDERID,
+    appId: process.env.REACT_APP_APPID,
+    measurementId: process.env.REACT_APP_MEASUREMENTID
+};
+
+console.log(firebaseConfig)
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
