95e1087cce4a1ca907823710397f11c3bc935027
Aria Kim <shoo7830@gmail.com>
2022-05-01 23:33:38 +0900

    update: title 컴포넌트 이름 변경 및 제작

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import firebase from './services/firebase';
 import './styles/App.scss';
 import Button from "@mui/material/Button";
+import Title from './components/RollingPaperTitle';
 
 function App() {
   return (
-    <Button variant='contained'>Hello World </Button>
+
+    <><Button variant='contained'>Hello World </Button><Title></Title></>
   );
 }
 
---
+++ src/components/RollingPaperTitle.jsx
@@ -0,0 +1,5 @@
+function Title() {
+    return <h2>Aria의 10주년을 축하해주세요.</h2>
+};
+
+export default Title;
