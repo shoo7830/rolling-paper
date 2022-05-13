168d42aad645898c5d4c604ef346e07ae649bad7
Aria Kim <shoo7830@gmail.com>
2022-05-13 14:04:25 +0900

    update:console.log삭제

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -12,7 +12,5 @@ const firebaseConfig = {
     measurementId: process.env.REACT_APP_MEASUREMENTID
 };
 
-console.log(firebaseConfig)
-
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
